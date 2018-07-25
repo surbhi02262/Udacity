aa69d9fe69ccf967eb6cf8d4bfe73be537cdec40
surbhi02262 <surbhi02262@gmail.com>
2018-07-25 16:51:38 +0530

    changes

---
+++ src/Form.js
@@ -0,0 +1,57 @@
+import React,{Component} from 'react'
+import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+
+class Form extends Component{
+state={
+	query: ''
+}
+
+updateQuery = (query) => {this.setState({query: query.trim()})}
+
+
+render(){
+	let showingContacts;
+	if(this.state.query){
+	const match = new RegExp(escapeRegExp(this.state.query),'i')
+	console.log('match is',match);
+	showingContacts = this.props.contact.filter((con) => match.test(con.name))
+	{console.log('showing',showingContacts)}
+	}
+	else
+	{
+		showingContacts =this.props.contacts
+	}
+	//{console.log('showingContacts'+JSON.stringify(showingContacts))}
+	return(
+		<div>
+		<div className='search-field'>
+			<input type='text' placeholder='Search Contacts'
+			 value={this.state.query} 
+			 onChange= {(event) => this.updateQuery(event.target.value)}/>
+		</div>
+			<ul className='contact-list'>
+		{this.props.contact.map((contact) => (
+		<li key ={contact.id} className='contact-list-item'>
+		<div className="contact-avatar" style={{
+			background: `url${contact.avatarURL}`
+		}}/>
+		<div className='listitems'>
+			<p>{contact.name}</p>
+			<p>{contact.email}</p>
+		</div>
+		<button className="remove-btn" 
+onClick = {()=> this.props.onDeleteContact(contact)}
+>Remove Contact</button>
+	</li>))}
+			</ul>
+</div>
+
+
+	);
+}
+
+}
+
+export default Form
---
+++ src/ListContacts.js
@@ -0,0 +1,31 @@
+import React,{Component} from 'react'
+import PropTypes from 'prop-types'
+
+
+function ListContacts(props){
+		return(
+<ul class='contact-list'>
+{console.log('prop is',props.contact)}
+	{props.contact.map((con) => (
+	<li key ={con.id} className='contact-list-item'>
+	<div className="contact-avatar" style={{
+		background: `url${con.avatarURL}`
+	}}>
+	<div className='listitems'>
+	<p>{con.name}</p>
+	<p>{con.email}</p>
+	</div>
+	</div>
+<button className="remove-btn" 
+onClick = {()=> props.onDeleteContact(con)}
+>Remove Contact</button>
+	</li>))}
+
+</ul>
+			);
+	}
+
+ListContacts.PropTypes={
+	contacts:PropTypes.array.isRequired
+}
+export default ListContacts
