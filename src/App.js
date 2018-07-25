48614c762694b913e6a47ca82bc759a91be63322
SauravVerma <saurav.verma@target.com>
2018-07-25 20:03:18 +0530

    dev branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
+import Form from './Form'
  
+
+
 class App extends Component {
+  state={
+contacts : [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+
+]
+}
+
+removeContact = (con) => {
+  this.setState((state) => ({
+contacts: state.contacts.filter((c) => c.id !== con.id)
+  })
+
+    )
+
+}
+
+
   render() {
     return (
-      <div>Contacts</div>
-    )
+   //   <ListContacts contact={this.state.contacts} onDeleteContact= {this.removeContact}/>
+    <Form contact={this.state.contacts} onDeleteContact= {this.removeContact}/>
+    );
   }
 }
 
-export default App
+export default App;
