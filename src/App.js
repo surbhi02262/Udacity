5eecd9f2e1cb9ca463a401a5502a5ed8a1ff52b9
SauravVerma <saurav.verma@target.com>
2018-07-25 19:57:32 +0530

    Project start

--- src/App.js
+++ src/App.js
@@ -1,50 +1,11 @@
 import React, { Component } from 'react';
-import ListContacts from './ListContacts';
-import Form from './Form'
  
-
-
 class App extends Component {
-  state={
-contacts : [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-
-]
-}
-
-removeContact = (con) => {
-  this.setState((state) => ({
-contacts: state.contacts.filter((c) => c.id !== con.id)
-  })
-
-    )
-
-}
-
-
   render() {
     return (
-   //   <ListContacts contact={this.state.contacts} onDeleteContact= {this.removeContact}/>
-    <Form contact={this.state.contacts} onDeleteContact= {this.removeContact}/>
-    );
+      <div>Contacts</div>
+    )
   }
 }
 
-export default App;
+export default App
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,31 +1,11 @@
-import React,{Component} from 'react'
-import PropTypes from 'prop-types'
+import React, {Component} from 'react'
 
-
-function ListContacts(props){
-		return(
-<ul class='contact-list'>
-{console.log('prop is',props.contact)}
-	{props.contact.map((con) => (
-	<li key ={con.id} className='contact-list-item'>
-	<div className="contact-avatar" style={{
-		background: `url${con.avatarURL}`
-	}}>
-	<div className='listitems'>
-	<p>{con.name}</p>
-	<p>{con.email}</p>
-	</div>
-	</div>
-<button className="remove-btn" 
-onClick = {()=> props.onDeleteContact(con)}
->Remove Contact</button>
-	</li>))}
-
-</ul>
-			);
-	}
-
-ListContacts.PropTypes={
-	contacts:PropTypes.array.isRequired
+function ListContacts(props) {
+  return (
+    <ul class='contact-list'>
+      <li>List</li>
+    </ul>
+  );
 }
+
 export default ListContacts
